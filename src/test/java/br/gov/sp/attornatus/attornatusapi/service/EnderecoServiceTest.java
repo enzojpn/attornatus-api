0c379c19b6d5b76c7689201dd8023c650e2649e7
enzojpn <enzo_jpn@hotmail.com>
2023-09-26 20:03:58 -0300

    Criando teste para EnderecoService quando nao envia pessoa

--- src/test/java/br/gov/sp/attornatus/attornatusapi/service/EnderecoServiceTest.java
+++ src/test/java/br/gov/sp/attornatus/attornatusapi/service/EnderecoServiceTest.java
@@ -12,6 +12,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
+import br.gov.sp.attornatus.attornatusapi.core.exception.PessoaNaoEncontradoException;
 import br.gov.sp.attornatus.attornatusapi.core.model.Endereco;
 import br.gov.sp.attornatus.attornatusapi.core.model.Pessoa;
 import br.gov.sp.attornatus.attornatusapi.core.repository.EnderecoRepository;
@@ -60,6 +61,55 @@ public class EnderecoServiceTest {
 		Assertions.assertEquals("curitiba", enderecoCriado.getCidade());
 		Assertions.assertEquals("22", enderecoCriado.getNumero());
 		Assertions.assertEquals("rua de teste", enderecoCriado.getLogradouro());
+
+	}
+
+	@Test
+	void deveRetornarPessoaNaoEncontradaException_QuandoEnviarEnderecoSemPessoa() throws Exception {
+ 
+		var endereco = new Endereco();
+		endereco.setId(1L);
+		endereco.setCep("02040033");
+		endereco.setCidade("curitiba");
+		endereco.setLogradouro("rua de teste");
+		endereco.setNumero("22"); 
+
+		Mockito.when(enderecoRepository.save(Mockito.any())).thenReturn(endereco);
+
+		Mockito.when(pessoaRepository.findById(Mockito.any())).thenReturn(Optional.empty());
+
+
+		Mockito.when(pessoaRepository.findById(Mockito.any())).thenReturn(Optional.empty());
+		Assertions.assertThrows(PessoaNaoEncontradoException.class, () -> {
+			 enderecoService.criarEndereco(endereco);
+		});
+
+	}
+	@Test
+	void deveRetornarPessoaNaoEncontradaException_QuandoNaoEncontrarPessoa() throws Exception {
+		var pessoa = new Pessoa();
+		pessoa.setId(1L);
+		pessoa.setNome("Silvio Santos");
+		Date dataNascimento = Date.from(Instant.parse("2003-11-19T00:00:00.000+00:00"));
+		pessoa.setDataNascimento(dataNascimento);
+
+		var endereco = new Endereco();
+		endereco.setId(1L);
+		endereco.setCep("02040033");
+		endereco.setCidade("curitiba");
+		endereco.setLogradouro("rua de teste");
+		endereco.setNumero("22");
+		endereco.setPessoa(pessoa);
+
+		Mockito.when(enderecoRepository.save(Mockito.any())).thenReturn(endereco);
+
+		Mockito.when(pessoaRepository.findById(Mockito.any())).thenReturn(Optional.empty());
+
+
+		Mockito.when(pessoaRepository.findById(Mockito.any())).thenReturn(Optional.empty());
+		Assertions.assertThrows(PessoaNaoEncontradoException.class, () -> {
+			 enderecoService.criarEndereco(endereco);
+		});
 
 	}
 
